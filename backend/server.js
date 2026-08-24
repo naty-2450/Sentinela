a616e83910753252d433353bcc696acb75ef4424
Nátaly Vitoria <118223377+naty-2450@users.noreply.github.com>
2026-08-24 11:22:51 -0300

    teste

--- backend/server.js
+++ backend/server.js
@@ -189,5 +189,6 @@ app.get("/medicacoes", (req, res) => {
 // START
 // START
 const PORT = process.env.PORT || 3000; 
-app.listen(PORT, () => {console.log(`Porta${PORT}`);
-                       });
+app.listen(PORT, "0.0.0.0", () => {
+  console.log(`Servidor rodando na porta ${PORT}`);
+});
