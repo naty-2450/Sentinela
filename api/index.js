e1f67e4167b2e635d69e3d5333af9f2131434a61
Nátaly Vitoria <118223377+naty-2450@users.noreply.github.com>
2026-08-24 11:29:15 -0300

    Acho que vai

--- api/index.js
+++ api/index.js
@@ -148,4 +148,8 @@ app.get("/medicacoes", (req, res) => {
 });
 
 // START
-module.exports = app;
+const PORT = process.env.PORT || 10000;
+
+app.listen(PORT, "0.0.0.0", () => {
+  console.log(`Servidor rodando na porta ${PORT}`);
+});
